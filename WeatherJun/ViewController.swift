2ca5db0f3b3a8ddcbd4c8c54c9ce0f33e854ffb8
yaSkazalGorbatiy <ya.skazal.gorbatiy@gmail.com>
2020-10-20 16:18:42 +0300

    Initial Commit

---
+++ WeatherJun/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherJun
+//
+//  Created by Сергей Горбачёв on 20.10.2020.
+//  Copyright © 2020 Сергей Горбачёв. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
